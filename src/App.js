ebebf2a5c74171d9eb01a279aefe11048e602a48
tendaitt <970497+tendaitt@users.noreply.github.com>
2020-04-25 09:24:29 +0200

    FEAT:Set up routing

--- src/App.js
+++ src/App.js
@@ -1,70 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import { Component } from 'react';
 import './App.css';
 import Home from './Home';
-
-export const authEndpoint = 'https://accounts.spotify.com/authorize?'
-
-const clientId = '';
-const redirectUri = '';
-const responseType = 'token';
-// TODO add state
-const scopes = [
-  'playlist-modify-public',
-  'playlist-modify-private'
-]
-
-const hash = window.location.hash.substring(1).split("&").reduce(function(initial, item) {
-  if (item) {
-    var parts = item.split("=");
-    initial[parts[0]] = decodeURIComponent(parts[1]);
-  }
-  return initial;
-}, {});
-
-window.location.hash = "";
+import Login from './Login';
+import Error from './Error';
 
 class App extends Component {
-
-  constructor() {
-
-    super();
-
-    this.state = {
-
-      token: null,
-    }
-  };
-
-  componentDidMount() {
-
-    let _accessToken = hash.access_token;
-
-    if (_accessToken) {
-
-      this.setState({
-        accessToken: _accessToken
-      });
-    }
-  };
-
+  
   render() {
-
+    
     return (
-
-      <div>
-        <header className="App-header">
-          {!this.state.token && (
-            <a href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=${responseType}&show_dialog=true`}>
-            Login to Spotify
-            </a>
-          )}
-          {this.state.token && (
-            <Home/>
-          )}
-        </header>
-      </div>
-
+      <main>
+        <Switch>
+          <Route exact path="/" component={Login}/>
+          <Route path="/home/:accessToken/:refreshToken" component={Home}/>
+          <Route path="/error/:errorMessage" component={Error}/>
+        </Switch>
+      </main>
     );
   }
 }

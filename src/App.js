50b996c1e66d0d959755f8a627bbebddf3f8d45a
tendaitt <970497+tendaitt@users.noreply.github.com>
2020-04-27 19:40:06 +0200

    FEAT:Add initial AppBar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Component } from 'react';
 import './App.css';
 import Home from './Home';
 import Login from './Login';
 import Error from './Error';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
-class App extends Component {
-  
-  render() {
-    
-    return (
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+function App(){
+
+  const classes2 = useStyles();
+
+  return (
       <main>
+        <AppBar position="static" style={{backgroundColor: '#101317'}}>
+          <Toolbar>
+            <Typography variant="h5" className={classes2.title}>The Wayback Song Machine</Typography>
+          </Toolbar>
+        </AppBar>
+
         <Switch>
           <Route exact path="/" component={Login}/>
           <Route path="/home/:accessToken/:refreshToken" component={Home}/>
@@ -19,7 +40,6 @@ class App extends Component {
         </Switch>
       </main>
     );
-  }
 }
 
 export default App;

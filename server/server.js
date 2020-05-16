52a818fce2ebcd471e29f4ab47686fa706dbc95f
tendaitt <970497+tendaitt@users.noreply.github.com>
2020-05-16 15:06:44 +0200

    FIX:Path to React files

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(cookieParser())
     .use('/', router);
 
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(app.get('port'), () => {

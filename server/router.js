313d2d7c7b474ab677011eb76fb82cdbf374694f
tendaitt <970497+tendaitt@users.noreply.github.com>
2020-05-22 11:25:03 +0200

    FIX:Linting issues

--- server/router.js
+++ server/router.js
@@ -1,6 +1,6 @@
 const Spotify = require('spotify-web-api-node');
 const express = require('express');
-const path = require('path');
+
 const router = new express.Router();
 
 const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID || '';
@@ -12,52 +12,53 @@ const CLIENT_REDIRECT_URI = process.env.SPOTIFY_CLIENT_REDIRECT_URI || 'http://l
 const scopes = ['playlist-modify-public', 'playlist-modify-private'];
 
 const spotifyApi = new Spotify({
-    clientId: CLIENT_ID,
-    clientSecret: CLIENT_SECRET,
-    redirectUri: REDIRECT_URI
-})
+  clientId: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  redirectUri: REDIRECT_URI,
+});
 
-const generateRandomString = function(length) {
-    var text = '';
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  
-    for (var i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
+const generateRandomString = function generateRandomString(length) {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
 };
 
 router.get('/api/login', (_, res) => {
-    const state = generateRandomString(16);
-    res.cookie(STATE_KEY, state);
-    res.redirect(spotifyApi.createAuthorizeURL(scopes, state));
+  const state = generateRandomString(16);
+  res.cookie(STATE_KEY, state);
+  res.redirect(spotifyApi.createAuthorizeURL(scopes, state));
 });
 
 router.get('/api/callback', (req, res) => {
-    const { code, state } = req.query;
-    const storedState = req.cookies ? req.cookies[STATE_KEY] : null;
+  const { code, state } = req.query;
+  const storedState = req.cookies ? req.cookies[STATE_KEY] : null;
 
-    // validate state
-    if(state === null || state !== storedState) {
-        res.redirect(`${CLIENT_REDIRECT_URI}/#/error/state mismatch`);
-    } else {
-        res.clearCookie(STATE_KEY);
-        spotifyApi.authorizationCodeGrant(code).then(data => {
-            const { access_token, refresh_token} = data.body;
+  // validate state
+  if (state === null || state !== storedState) {
+    res.redirect(`${CLIENT_REDIRECT_URI}/#/error/state mismatch`);
+  } else {
+    res.clearCookie(STATE_KEY);
+    spotifyApi.authorizationCodeGrant(code).then((data) => {
+      const { accessToken, refreshToken } = data.body;
 
-            // set the access token on the API object to use it in later calls
-            spotifyApi.setAccessToken(access_token);
-            spotifyApi.setRefreshToken(refresh_token);
+      // set the access token on the API object to use it in later calls
+      spotifyApi.setAccessToken(accessToken);
+      spotifyApi.setRefreshToken(refreshToken);
 
-            // use the access token to access the Spotify Web API
-            spotifyApi.getMe().then(({ body }) => {
-                console.log(body);
-            });
-            res.redirect(`${CLIENT_REDIRECT_URI}/home/${access_token}/${refresh_token}`);
-        }).catch(err => {
-            res.redirect(`${CLIENT_REDIRECT_URI}/error/invalid token`);
-        });
-    }
-})
+      // use the access token to access the Spotify Web API
+      spotifyApi.getMe().then(({ body }) => {
+        console.log(body);
+      });
+      res.redirect(`${CLIENT_REDIRECT_URI}/home/${accessToken}/${refreshToken}`);
+    }).catch((err) => {
+      console.log(err);
+      res.redirect(`${CLIENT_REDIRECT_URI}/error/invalid token`);
+    });
+  }
+});
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,9 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const path = require('path');
+const morgan = require('morgan');
 const router = require('./router');
-
-var morgan = require('morgan')
 
 const port = process.env.PORT || 5000;
 
@@ -12,16 +11,16 @@ const app = express();
 
 app.set('port', port);
 app.use(cookieParser())
-    .use(bodyParser.json())
-    .use(bodyParser.urlencoded({ extended: false }))
-    .use(express.static(path.resolve(__dirname, '../client/build')))
-    .use(morgan('tiny'))
-    .use('/', router);
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: false }))
+  .use(express.static(path.resolve(__dirname, '../client/build')))
+  .use(morgan('tiny'))
+  .use('/', router);
 
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(app.get('port'), () => {
-    console.log('Express server listening on port ' + app.get('port'));
+  console.log(`Express server listening on port ${app.get('port')}`);
 });
